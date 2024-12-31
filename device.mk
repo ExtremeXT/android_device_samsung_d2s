a20eb361222e446b0b7a72114c412472a78ff6da
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2024-12-31 15:43:05 +0200

    device.mk: remove redundant fastboot line
    * Not needed for twrp-12.1
    
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -20,7 +20,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := false
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
 
 PRODUCT_PRODUCT_PROPERTIES += \
